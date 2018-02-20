1e1dffb2ddcf49713dc501f68ae427e1822a7846
DiscRiskandBisque <joshquin@gmail.com>
2018-02-20 14:19:49 -0500

    Readme tweaks, cacheSend() returns stackId

--- src/DrizzleContract.js
+++ src/DrizzleContract.js
@@ -107,20 +107,8 @@ class DrizzleContract {
       // When txhash received, will be value of stack ID
       contract.store.dispatch({type: 'SEND_CONTRACT_TX', contract, fnName, fnIndex, args, stackId})
      
-      // return promise that observes store, checks when stack ID has tx ID
-      // resolve tx ID
-      // Destroy store listener
-      return new Promise((resolve, reject) => {
-        let unsubscribe = contract.store.subscribe(() => {
-          var stackHash = contract.store.store.getState().transactionStack[stackId]
-
-          if (stackHash) {
-            resolve(stackHash)
-          }
-        })
-
-        unsubscribe()
-      })
+      // return stack ID
+      return stackId
     }
   }
 
