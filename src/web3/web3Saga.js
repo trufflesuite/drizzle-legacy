bb2b84aeaa08df4f598a78264312948b086dd8cd
Tarrence van As <tarrence13@gmail.com>
2018-04-08 13:21:22 -0700

    gitignore compiled code

--- src/web3/web3Saga.js
+++ src/web3/web3Saga.js
@@ -10,7 +10,7 @@ export function* initializeWeb3({options}) {
   var web3 = {}
 
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof window.web3 !== 'undefined' && false) {
+  if (typeof window.web3 !== 'undefined') {
     // Use Mist/MetaMask's provider.
     web3 = new Web3(window.web3.currentProvider)
     web3.eth.cacheSendTransaction = (txObject) => put({type: 'SEND_WEB3_TX', txObject, stackId, web3})
