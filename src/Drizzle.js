f4be263fc50968927b0ffd8185055ecd7ae95179
Chris <git@chris-ip.com>
2018-12-19 13:09:03 +0200

    check if window object exist or replace with "global"

--- src/Drizzle.js
+++ src/Drizzle.js
@@ -2,10 +2,12 @@ import { generateStore } from './generateStore'
 
 // Load as promise so that async Drizzle initialization can still resolve
 var windowPromise = new Promise((resolve, reject) => {
-  window.addEventListener('load', resolve)
-  
-  // resolve in any case if we missed the load event and the document is already loaded
-  if (document.readyState === `complete`) resolve()
+  if (typeof window !== 'undefined') {
+    window.addEventListener('load', resolve)
+
+    // resolve in any case if we missed the load event and the document is already loaded
+    if (document.readyState === `complete`) resolve()
+  }
 })
 
 class Drizzle {
