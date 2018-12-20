24384902c1cd7bad0fdd3c88c5e809035b9defa5
DiscRiskandBisque <joshquin@gmail.com>
2018-12-20 17:17:48 -0500

    accountBalances, web3 saga tests

--- test/accounts.test.js
+++ test/accounts.test.js
@@ -1,21 +1,21 @@
 import { getAccounts } from '../src/accounts/accountsSaga';
 import Ganache from 'ganache-cli';
 import Web3 from 'web3';
-import { runSaga } from 'redux-saga'
+import { runSaga } from 'redux-saga';
+//import configureStore from 'redux-mock-store';
 
-var provider;
 var web3;
 
 var dispatchedActions;
 var store;
 
 beforeAll(() => {
-  provider = Ganache.provider({seed: "drizzle", gasLimit: 7000000});
-  web3 = new Web3(provider);
+  //provider = Ganache.provider({seed: "drizzle", gasLimit: 7000000});
+  web3 = new Web3('http://127.0.0.1:7545');
 
   dispatchedActions = [];
   store = {
-    getState: () => {},
+    getState: () => ({}),
     dispatch: action => dispatchedActions.push(action)
   };
 });
