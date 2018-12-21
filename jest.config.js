e6ba3a6f21fd4aec3d039cbe723cc9534ff72353
DiscRiskandBisque <joshquin@gmail.com>
2018-12-21 02:46:33 -0500

    Setup ganache environment

--- jest.config.js
+++ jest.config.js
@@ -126,7 +126,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  testEnvironment: "<rootDir>/test/environments/ganache-environment.js",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
