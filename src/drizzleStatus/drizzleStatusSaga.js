40a54b75928c81b997a9c55271a37c8366d9dd89
Sean Casey <sean@seanjcasey.com>
2019-07-18 23:30:08 -0400

    remove uncommented code

--- src/drizzleStatus/drizzleStatusSaga.js
+++ src/drizzleStatus/drizzleStatusSaga.js
@@ -73,7 +73,6 @@ export function * initializeDrizzle (action) {
     }
   } catch (error) {
     yield put({ type: 'DRIZZLE_FAILED', error })
-    // yield put({ type: 'DRIZZLE_INITIALIZED' })
     console.error('Error initializing Drizzle:')
     console.error(error)
 
